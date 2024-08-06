c8e17942a1e95f868bd8379540e38004dfade51d
Daniel Morrow <daniel.morrow@kainos.com>
2024-08-06 11:36:35 -0400

    Added solutions for workbook 2 along with additional content

---
+++ Workbook 2 Solutions/app.js
@@ -0,0 +1,11 @@
+function changeBackgroundColour(colour){
+    document.body.style.backgroundColor = colour;
+}
+
+function changeParagraphColour(colour){
+    document.getElementById("paragraph1").style.color = colour;
+}
+
+function changeHeadingColour(colour){
+    document.getElementById("heading1").style.color = colour;
+}
