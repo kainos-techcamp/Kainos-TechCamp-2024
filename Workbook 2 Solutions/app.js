a10d0fb2985b8387f9cc79e83d04d33d731f9ecf
Daniel Morrow <daniel.morrow@kainos.com>
2024-08-08 10:47:00 -0400

    Minor solution touch ups

--- Workbook 2 Solutions/app.js
+++ Workbook 2 Solutions/app.js
@@ -9,3 +9,7 @@ function changeParagraphColour(colour){
 function changeHeadingColour(colour){
     document.getElementById("heading1").style.color = colour;
 }
+
+function hideHeading(){
+    document.getElementById("heading1").style.display = "none";
+}
